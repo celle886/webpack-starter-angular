21229bc40af4b866de58ec8b03f5cf668b089576
celle886 <info@marcelquasnica.de>
2016-06-20 07:22:20 +0200

    clean up

--- src/common/common.js
+++ src/common/common.js
@@ -1,10 +1,5 @@
 import angular from 'angular';
-import Navbar from './navbar/navbar';
-import Hero from './hero/hero';
-import User from './user/user';
 
 export default angular.module('app.common', [
-  Navbar.name,
-  Hero.name,
-  User.name
+
 ]);
